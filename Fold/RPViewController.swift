411a89db2cf3e1d57a58e044ba8405fb4fa8d709
danielzuot <danielzuot@gmail.com>
2016-01-17 23:28:37 -0500

    uuids in constant for peripehralmanager

--- Fold/RPViewController.swift
+++ Fold/RPViewController.swift
@@ -12,9 +12,6 @@ import CoreBluetooth
 class RPViewController: UIViewController, CBPeripheralManagerDelegate, UITextViewDelegate {
 
     @IBOutlet weak var amountRequested: UITextField!
-    //constants
-    let testServiceUUID = CBUUID(string: "56054B44-6C60-4D42-BAB3-7D1AB28498C6")
-    let testCharacteristicUUID = CBUUID(string: "54D6C478-5008-4F9B-8DEB-D28A4E62AADB")
     
     //globals
     var peripheralManager : CBPeripheralManager!
@@ -28,9 +25,9 @@ class RPViewController: UIViewController, CBPeripheralManagerDelegate, UITextVie
     }
     
     override func viewWillDisappear(animated: Bool) {
-        super.viewWillDisappear(animated)
         NSLog("Broadcast stopped.")
         peripheralManager.stopAdvertising()
+        super.viewWillDisappear(animated)
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,7 +37,7 @@ class RPViewController: UIViewController, CBPeripheralManagerDelegate, UITextVie
     
     @IBAction func broadcastTapped(sender: AnyObject) {
         NSLog("Starting to broadcast...")
-        peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey : testServiceUUID])
+        peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey : CBUUID(string: testServiceUUID)])
     }
     
     @IBAction func requestedAmountUpdated(sender: AnyObject) {
@@ -63,20 +60,26 @@ class RPViewController: UIViewController, CBPeripheralManagerDelegate, UITextVie
         if peripheral.state != CBPeripheralManagerState.PoweredOn {
             NSLog("Bluetooth switched off or not initialized")
             return
-        } else {
-            //TODO change initial value to value in text field
-            NSLog("Creating service/characteristic tree...")
-            self.testCharacteristic = CBMutableCharacteristic(type: testCharacteristicUUID, properties: CBCharacteristicProperties.Notify, value: nil, permissions: CBAttributePermissions.Readable)
-            let testService = CBMutableService(type: testServiceUUID, primary: true)
-            testService.characteristics = [self.testCharacteristic]
-            peripheralManager.addService(testService)
         }
+        //TODO change initial value to value in text field
+        NSLog("Creating service/characteristic tree...")
+        
+        self.testCharacteristic = CBMutableCharacteristic(type: CBUUID(string: testCharacteristicUUID), properties: CBCharacteristicProperties.Notify, value: nil, permissions: CBAttributePermissions.Readable)
+        let testService = CBMutableService(type: CBUUID(string: testServiceUUID), primary: true)
+        
+        testService.characteristics = [self.testCharacteristic]
+        peripheralManager.addService(testService)
+        
     }
     
     func peripheralManager(peripheral: CBPeripheralManager, central: CBCentral, didSubscribeToCharacteristic characteristic: CBCharacteristic) {
         NSLog("User has subscribed, updating amount requested value...")
         let amountReqData = (amountRequested.text! as NSString).dataUsingEncoding(NSUTF8StringEncoding)
         peripheralManager.updateValue(amountReqData!, forCharacteristic: self.testCharacteristic, onSubscribedCentrals: nil)
+    }
+    
+    func peripheralManager(peripheral: CBPeripheralManager, central: CBCentral, didUnsubscribeFromCharacteristic characteristic: CBCharacteristic) {
+        NSLog("User unsubscribed from vendor characteristic...")
     }
     
     /*
