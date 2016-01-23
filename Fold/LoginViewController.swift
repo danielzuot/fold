fc41f5a87cebc42440a73eecc9f3278d5079b9bc
danielzuot <danielzuot@gmail.com>
2016-01-23 13:15:14 -0500

    removed bridging file

--- Fold/LoginViewController.swift
+++ Fold/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import coinbase_official
 
 class LoginViewController: UIViewController {
 
--- Fold/NavViewController.swift
+++ Fold/NavViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class NavViewController: UINavigationController {
 
     override func viewDidLoad() {
