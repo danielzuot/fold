5f5cbe2482aab09075048ec952f22ea4784a3d97
danielzuot <danielzuot@gmail.com>
2016-02-01 00:04:54 -0500

    updated scopes to v1 and added constraints

--- Fold/Constants.swift
+++ Fold/Constants.swift
@@ -27,8 +27,8 @@ let addressUUID = CBUUID(string: ADDRESS_CHARACTERISTIC_UUID)
 
 let CLIENT_ID = "fa908ac547fc45176e3e735851cd6c504fd853cf1f0d91e2a5192d9fa6f3e89a"
 let CLIENT_SECRET = "03dcc5dde881365e012ae61da754b63b41371d2bf5b0c9712b92d93c522c1b2b"
-let USER_SCOPE = "wallet:accounts:read wallet:accounts:update wallet:accounts:create wallet:accounts:delete wallet:addresses:read wallet:addresses:create wallet:buys:read wallet:buys:create wallet:deposits:read wallet:deposits:create wallet:notifications:read wallet:payment-methods:read wallet:payment-methods:delete wallet:payment-methods:limits wallet:sells:read wallet:sells:create wallet:transactions:read wallet:transactions:send wallet:transactions:request wallet:transactions:transfer wallet:user:read wallet:user:update wallet:user:email wallet:withdrawals:read wallet:withdrawals:create"
-let VENDOR_SCOPE = ""
+let USER_SCOPE = "merchant balance addresses buttons buy contacts orders sell transactions send request transfer transfers user recurring_payments oauth_apps reports deposit withdraw"
+let VENDOR_SCOPE = "merchant balance addresses buttons buy contacts orders sell transactions send request transfer transfers user recurring_payments oauth_apps reports deposit withdraw"
 let USER_META = ["send_limit_amount": "50", "send_limit_currency": "USD", "send_limit_period": "day"]
 let VENDOR_META = ["send_limit_amount": "50", "send_limit_currency": "USD", "send_limit_period": "day"]
 let REDIRECT_URI = "com.fold.app.coinbase-oath://coinbase-oauth"
